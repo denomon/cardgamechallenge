be21b6ebe2eca2113750065cea1c6a5e8b8d7dde
Monari <MonaDe01@risk.regn.net>
2022-04-29 11:12:14 +0100

    code update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="hello">
+  <div class="hello" id="home">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>Is the next card Greater than the card dispalyed?</h2>
+    <hr/>
+    <h3>{{ playerMessage }}</h3>
+    <button v-if="playerLostGame || PlayerCardScoreValues.length == toWinScore " type="button" value="Reload Page" onclick="window.location.reload()"> Restart</button>
+
+
+    <!-- Calling function in methods -->
+    <div class="buttons">
+      <button @click="no">No!</button>
+      <button @click="yes">Yes!</button>
+    </div>
+    <div v-if="PlayerCardScoreValues.length < toWinScore">
+      <div class="card center">
+        <span class="card-value-suit top">{{ currentCardValue }}</span>
+        <span class="card-value-suit bot">{{ currentCardValue }}</span>
+      </div>
+    </div>
+    <div v-if="PlayerCardScoreValues.length === toWinScore" class="card center">
+      <h4>Your Winning Selection is shown below</h4>
+
+      {{
+        PlayerCardScoreValues.forEach((value, index) => {
+          appendWinnerCards(value);
+        })
+      }}
+    </div>
+
+    <h3>SCORE: {{ PlayerCardScoreValues.length }}</h3>
+
+    <hr/>
+
   </div>
 </template>
 
@@ -35,7 +40,99 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  el: "#home",
+  data() {
+    return {
+      cardValues: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+      counter: 0,
+      playerLostGame: false,
+      toWinScore: 1,
+      currentCardValue: 0,
+      cardStack: [],
+      nextCardValue: 0,
+      playerMessage: 'Select below to find out!',
+      PlayerCardScoreValues: []
+    };
+  },
+  methods: {
+    // Creating function
+
+    createStack: function () {
+      for (var decknumber = 0; decknumber <= 51; decknumber++) {
+        var chosenNumber = Math.floor(Math.random() * this.cardValues.length);
+        this.cardStack[decknumber] = this.cardValues[chosenNumber];
+      }
+    },
+    show: function () {
+      //Create the stack
+      this.createStack();
+      this.currentCardValue = this.cardStack[0];
+    },
+    yes: function () {
+      this.counter++;
+      console.log(this.cardStack);
+      //check if the next card value is highyer than current
+      this.nextCardValue = this.cardStack[this.counter];
+
+      //Check if they have scored or not
+      if (this.cardValues.indexOf(this.nextCardValue) > this.cardValues.indexOf(this.currentCardValue)) {
+        this.score++;
+        this.playerMessage = (this.PlayerCardScoreValues.length == this.toWinScore) ? 'You have WON the Game' : 'Good try';
+        this.PlayerCardScoreValues.push(this.currentCardValue);
+
+      } else if (this.cardValues.indexOf(this.nextCardValue) == this.cardValues.indexOf(this.currentCardValue)) {
+        this.playerLostGame = true;
+        this.playerMessage = 'You have lost the GAME!';
+        this.PlayerCardScoreValues.length = 0;
+      } else {
+        this.playerLostGame = true;
+        this.playerMessage = 'You have lost the GAME!';
+        this.PlayerCardScoreValues.length = 0;
+      }
+      this.currentCardValue = this.nextCardValue;
+    },
+    no: function () {
+      this.counter++;
+
+      //check if the next card value is highyer than current
+      this.nextCardValue = this.cardStack[this.counter];
+
+      //Check if they have scored or not
+      if (this.cardValues.indexOf(this.nextCardValue) < this.cardValues.indexOf(this.currentCardValue)) {
+        this.score++;
+        this.playerMessage = (this.PlayerCardScoreValues.length == this.toWinScore) ? 'You have WON the Game' : 'Good try';
+        this.PlayerCardScoreValues.push(this.currentCardValue);
+
+      } else if (this.cardValues.indexOf(this.nextCardValue) == this.cardValues.indexOf(this.currentCardValue)) {
+        this.playerLostGame = true;
+        this.playerMessage = 'You have lost the GAME!';
+        this.PlayerCardScoreValues.length = 0;
+      } else {
+        this.playerLostGame = true;
+        this.playerMessage = 'You have lost the GAME!';
+        this.PlayerCardScoreValues.length = 0;
+      }
+      this.currentCardValue = this.nextCardValue;
+
+    },
+    appendWinnerCards: function (scoredCardValue) {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.classList.add("center");
+      card.innerHTML =
+          '<span class="card-value-suit top">' + scoredCardValue + '</span>' +
+          '<span class="card-value-suit bot">' + scoredCardValue + '</span>';
+      document.body.appendChild(card);
+    }
+
+  },
+  refreshPage: function () {
+    this.$forceUpdate()
+  },
+  beforeMount() {
+    this.show()
+  },
 }
 </script>
 
@@ -44,15 +141,19 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+hr {
+  border: 0;
+  border-top: 2px solid brown;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.center {
+  margin: 0 auto;
+  text-align: center;
 }
-a {
-  color: #42b983;
+
+.buttons {
+  margin: 40px;
+
 }
 </style>
